65282e5aa2ccdab9a145c43db852305438822583
Matthias Baer <matthias.baer@mpl.mpg.de>
2021-01-19 10:07:47 +0100

    Resolve flake8 issues

--- tests/gui/test_main.py
+++ tests/gui/test_main.py
@@ -8,9 +8,10 @@ def test_main_window_can_activate_all_tabs(qtbot):
 
     window = BMicro()
     qtbot.add_widget(window)
-    tab_texts = ['Data', 'Extraction', 'Calibration', 'Peak Selection', 'Evaluation']
-    tab_names = ['tab_data', 'tab_extraction', 'tab_calibration', 'tab_peak_selection',
-                          'tab_evaluation']
+    tab_texts = ['Data', 'Extraction', 'Calibration',
+                 'Peak Selection', 'Evaluation']
+    tab_names = ['tab_data', 'tab_extraction', 'tab_calibration',
+                 'tab_peak_selection', 'tab_evaluation']
 
     for idx, (tab_text, tab_name) in enumerate(zip(tab_texts, tab_names)):
         current_tab = window.tabWidget.findChild(QWidget, tab_name)
