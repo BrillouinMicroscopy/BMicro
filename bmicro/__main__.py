e20f4a06eb0bbe99f71d9420b70acf95bd4d734c
Paul Müller <dev@craban.de>
2021-02-02 01:05:39 +0100

    ui: add app icon (#9)

--- bmicro/__main__.py
+++ bmicro/__main__.py
@@ -1,6 +1,8 @@
+import os
+import pkg_resources
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtGui, QtWidgets
 
 from .gui.main import BMicro
 
@@ -10,6 +12,11 @@ def main():
     Starts the BMicro application and handles its life cycle.
     """
     app = QtWidgets.QApplication(sys.argv)
+    # set window icon
+    imdir = pkg_resources.resource_filename("bmicro", "img")
+    icon_path = os.path.join(imdir, "icon.png")
+    app.setWindowIcon(QtGui.QIcon(icon_path))
+
     main = BMicro()
     main.show()
     sys.exit(app.exec_())
