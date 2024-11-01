1f459b6bdb0f34f8e907a6d1e0be544e5b2776df
Raimund Schlüßler <rs@cellsense-technologies.com>
2024-11-01 10:24:15 +0100

    chore: update to scipy 1.14.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
                       "pytest-qt",
                       "pytest_mock",
                       "scikit-image",
-                      "scipy==1.13.1"
+                      "scipy==1.14.1"
                       ],
     python_requires=">=3.9",
     keywords=["Brillouin Microscopy"],
